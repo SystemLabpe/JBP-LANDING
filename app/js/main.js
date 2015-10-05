41e47baf53d080af6a9e14c5ff7877324b15ef18
josuebarriosm <josue.barrios@systemlab.com.pe>
2015-10-05 11:41:39 -0500

    Update gallery

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,36 @@
 
 jQuery(document).ready(function($){
 
+  // quicksand
+  $('#filterOptions li.active a').attr('class');
+
+  var $holder = $('#item-list');
+
+  var $data = $holder.clone();
+
+  $('#filterOptions li a').click(function() {
+
+    $('#filterOptions li').removeClass('active');
+
+    var $filterType = $(this).attr('class');
+    $(this).parent().addClass('active');
+    var $filteredData;
+    if ($filterType === 'todos') {
+      $filteredData = $data.find('article');
+    }
+    else {
+      $filteredData = $data.find('article[data-type=' + $filterType + ']');
+    }
+
+    $holder.quicksand($filteredData, {
+      duration: 800,
+      easing: 'easeInCubic',
+      adjustHeight: false,
+      adjustWidth :false
+    });
+
+    return false;
+  });
+  //end quicksand
+
 });
